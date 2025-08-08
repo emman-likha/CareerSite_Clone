26f4c82eb2793c128723ff7017b619d22e947452
bulaonjohnemm4nuel.21@gmail.com <bulaonjohnemm4nuel.21@gmail.com>
2025-08-09 00:12:18 +0800

    continuation

---
+++ app/Users/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ProductCard from '../components/ProductCard'
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage
+    </div>
+  )
+}
+
+export default UsersPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
-   <main><h1>Hello World</h1></main>
+   <main><h1>Hello World</h1>
+    <Link href="/Users">Users</Link>
+    <ProductCard/>
+   </main>
   );
 }
